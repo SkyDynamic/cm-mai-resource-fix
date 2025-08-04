d70be1769f9f435c26c908391711ab9c0fd06b41
SkyDynamic <tcplxw@outlook.com>
2025-08-05 02:21:44 +0800

    1

---
+++ CmMaiResourceFix.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using BepInEx;
+using HarmonyLib;
+
+namespace cm_mai_resource_fix
+{
+
+    [BepInPlugin("CmMaiResourceFix", "CmMaiResourceFix", "1.0.0.0")]
+    public class CmMaiResourceFix : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony("CmMaiResourceFix");
+
+        void Awake()
+        {
+            Logger.LogInfo("Load Mod");
+            harmony.PatchAll();
+
+            var patchedMethods = harmony.GetPatchedMethods().ToList();
+            Logger.LogInfo($"Number of patched methods: {patchedMethods.Count}");
+
+            foreach (var method in patchedMethods)
+            {
+                Logger.LogInfo($"Patched method: {method.DeclaringType?.Name}.{method.Name}");
+            }
+        }
+
+        void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+    }
+}
---
+++ MaiResourcePatch.cs
@@ -0,0 +1,15 @@
+﻿using CardMaker.Common;
+using HarmonyLib;
+
+namespace cm_mai_resource_fix
+{
+    public class MaiResourceManagerPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(AssetBundleDB), "getCardAssetFileNameWithDetail")]
+        public static void GetCardAssetFileNameWithDetail_Postfix(string __result)
+        {
+            CmMaiResourceFix.logger.LogInfo(__result);
+        }
+    }
+}
