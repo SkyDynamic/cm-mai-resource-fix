1f074f53cfe89c37e58d64ffbe3e0ca24862f733
SkyDynamic <tcplxw@outlook.com>
2025-08-05 20:49:18 +0800

    不从RomConfig获取Option位置

--- MaiResourcePatch.cs
+++ MaiResourcePatch.cs
@@ -3,28 +3,19 @@ using System.IO;
 using System.Runtime.InteropServices;
 using CardMaker.Common;
 using CardMaker.MAI;
+using AMDaemon;
 using HarmonyLib;
 
 namespace cm_mai_resource_fix
 {
     public class MaiResourcePatch
     {
-        private static CardMaker.MAI.MaiStudio.RomConfig romConfig;
-        
-        [HarmonyPostfix]
-        [HarmonyPatch(typeof(MAIDataManager.Loader), "mount")]
-        public static void mount_Postfix(MAIDataManager.Loader __instance, ref bool __result)
-        {
-            var romConfigField = AccessTools.Field(typeof(MAIDataManager.Loader), "romConfig_");
-            romConfig = (CardMaker.MAI.MaiStudio.RomConfig)romConfigField.GetValue(__instance);
-        }
-        
         [HarmonyPostfix]
         [HarmonyPatch(typeof(MAIContext), "Awake")]
         public static void Awake_Postfix(MAIContext __instance)
         {
             var list = new List<Entry>();
-            Find(list, romConfig.optDir, "/MAI");
+            Find(list, AppImage.OptionMountRootPath, "/MAI");
             for (int index = 0; index < list.Count; ++index)
             { 
                 if (list[index].type_ == Entry.Type_OS)
